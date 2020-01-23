a112c02c83260f350e99fb4c5446f0d8883961a7
KC <54818691+jslandport@users.noreply.github.com>
2020-01-23 13:40:11 -0500

    Update urls.py

--- arttherapy/trackart/urls.py
+++ arttherapy/trackart/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
 	path('viewclients', views.ClientCentricListView.as_view()),
 	path('aptmonth', views.AppointmentCentricListView.as_view()),
 	
-	path('client/<int:art_client_id>', views.ClientOneView.as_view(), name='viewoneclient')
+	## view 1 record:
+	path('client/<int:art_client_id>', views.ClientOneView.as_view()),
+	path('appointment/<int:art_appointment_id>', views.AppointmentOneView.as_view()),
+	path('painting/<int:art_painting_id>', views.PaintingOneView.as_view())
+	
+	## add/edit records(?)
 ]
 
 ###path('client/<int:art_client_id>', views.ClientOneView.as_view()),
