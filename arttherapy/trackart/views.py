77903f0ce6cc37a0f8ba19c6b706f12629452a8d
KC <54818691+jslandport@users.noreply.github.com>
2020-01-23 13:39:52 -0500

    Update views.py

--- arttherapy/trackart/views.py
+++ arttherapy/trackart/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 ## from .forms import ArtTherapyForm
 ## from trackart import templates
 from .models import art_client
+from .models import art_appointment
+from .models import art_painting
 
 
 # Create your views here.
@@ -35,19 +37,58 @@ class ClientCentricListView(TemplateView):
 
 class AppointmentCentricListView(TemplateView):
    def get(self, request, **kwargs):
-      return render(request, 'trackart/appointmentlist.html') ##, context=context
+      qsappointment = art_appointment.objects.all().order_by('art_appointmenttime')
+      context = {
+         'qsappointment': qsappointment
+      }
+      print(qsappointment)
+      return render(request, 'trackart/appointmentlist.html', context=context)
+
 
 
 class ClientOneView(TemplateView):
    def get(self, request, art_client_id):
-      qsclient = art_client.objects.all() ##pk=art_client_id
+      qsclient = art_client.objects.filter(pk=art_client_id)
       if len(qsclient) == 1:
          qrow = qsclient[0]
+         qsapt = art_appointment.objects.filter(art_clientid = art_client_id)
          context = {
             'qoneclient': qrow,
-            'id': art_client_id
+            'qsappointment': qsapt,
+            'pkid': art_client_id
          }
       else:
          context = {}
       return render(request, 'trackart/clientone.html', context=context)
 
+
+class AppointmentOneView(TemplateView):
+   def get(self, request, art_appointment_id):
+      qsappointment = art_appointment.objects.filter(pk=art_appointment_id)
+      if len(qsappointment) == 1:
+         qrow = qsappointment[0]
+         qspainting = art_painting.objects.filter(art_appointmentid = art_appointment_id)
+         context = {
+            'qoneappointment': qrow,
+            'qspainting': qspainting,
+            'pkid': art_appointment_id
+         }
+      else:
+         context = {}
+      return render(request, 'trackart/appointmentone.html', context=context)
+
+
+class PaintingOneView(TemplateView):
+   def get(self, request, art_painting_id):
+      qspainting = art_painting.objects.filter(pk=art_painting_id)
+      if len(qspainting) == 1:
+         qrow = qspainting[0]
+         ### qspainting = art_painting.objects.filter(art_paintingid = art_painting_id)
+         context = {
+            'qonepainting': qrow,
+            ###'qspainting': qspainting,
+            'pkid': art_painting_id
+         }
+      else:
+         context = {}
+      return render(request, 'trackart/paintingone.html', context=context)
