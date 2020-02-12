3fb3fcd2055342981ec4aaf8071e9b7e4c663820
KC <54818691+jslandport@users.noreply.github.com>
2020-02-12 14:24:48 -0500

    Update admin.py

--- arttherapy/trackart/admin.py
+++ arttherapy/trackart/admin.py
@@ -9,7 +9,6 @@ from .models import art_client, art_appointment, art_painting
 ## lookups
 from .models import paintcolor, clientmood
 ## many x many-s
-from .models import art_paintingXpaintcolor, art_paintingXclientmood
 
 admin.site.register(art_client)
 admin.site.register(art_appointment)
@@ -18,5 +17,3 @@ admin.site.register(art_painting)
 admin.site.register(paintcolor)
 admin.site.register(clientmood)
 
-admin.site.register(art_paintingXpaintcolor)
-admin.site.register(art_paintingXclientmood)
