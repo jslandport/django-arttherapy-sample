7a07099eff951d4014e6f42cd72dad9420463c52
KC <54818691+jslandport@users.noreply.github.com>
2020-02-14 11:23:04 -0500

    Update navhelper.py

--- arttherapy/trackart/navhelper.py
+++ arttherapy/trackart/navhelper.py
@@ -4,11 +4,21 @@ def getnavdictfromparamsdict(dparams, cururl):
       if thiskey == 'art_clientid':
          ## should we link to this client?
          if dparams[thiskey] > 0:
-            clink = '/client/' + str(dparams[thiskey])
-            if cururl != clink:
-               dnav[clink] = 'View Client'
+            thislink = '/client/' + str(dparams[thiskey])
+            if cururl != thislink:
+               dnav[thislink] = 'This Client'
          ## link to the List-of-clients
-         dnav['/viewclients'] = 'Client-Centric View'
+         dnav['/viewclients'] = 'All Clients'
       elif thiskey == 'art_appointmentid':
-         dnav['/viewappointments'] = 'Appointment-Centric View'
+         if dparams[thiskey] > 0:
+            thislink = '/appointment/' + str(dparams[thiskey])
+            if cururl != thislink:
+               dnav[thislink] = 'This Appointment'
+         dnav['/viewappointments'] = 'All Appointments'
+      elif thiskey == 'art_paintingid':
+         if dparams[thiskey] > 0 & dparams['art_appointmentid'] > 0:
+            thislink = '/appointment/' + dparams['art_appointmentid'] + '/painting/' + str(dparams[thiskey])
+            if cururl != thislink:
+               dnav[thislink] = 'This Painting'
+         dnav['/viewappointments'] = 'All Paintings'
    return dnav
