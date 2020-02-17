5ab76dcae91cce5f56f5f3c722385b0c1d997ad2
KC <54818691+jslandport@users.noreply.github.com>
2020-02-17 12:35:11 -0500

    putting html5 datetime functionality in

--- arttherapy/trackart/forms.py
+++ arttherapy/trackart/forms.py
@@ -4,7 +4,16 @@ from .models import paintcolor
 from .models import clientmood
 
 
-### mytuple = [('BF', 'before'), ('DU','during'), ('AF','after')]
+'''
+as per dev.to/djangotricks, this kind of approach is how you create an input behavior such that you can get, for example, HTML5's <input type="datetime-local".. into your django forms:
+'''
+class DateTimeInput_forhtml5(forms.DateTimeInput):
+    input_type = "datetime-local"
+
+    def __init__(self, **kwargs):
+        kwargs["format"] = "%Y-%m-%dT%H:%M" ## this is the kind of format that datetime-local wants
+        super().__init__(**kwargs)
+
 
 class ClientForm(forms.Form):
    ## d = {"one": 1, "two": 2, "Three":3 }
@@ -13,8 +22,7 @@ class ClientForm(forms.Form):
    art_clientdob = forms.DateField(
       label='Date of Birth',
       input_formats=[
-         '%m/%d/%Y',
-         '%m-%d-%Y'
+         '%m/%d/%Y'
       ],
       widget=forms.widgets.DateInput(format="%m/%d/%Y")
    )
@@ -27,10 +35,9 @@ class AppointmentForm(forms.Form):
    art_appointmenttime = forms.DateTimeField(
       label='Appointment Date/Time',
       input_formats=[
-         '%m/%d/%Y %H:%M',
-         '%m-%d-%Y %H:%M'
+         '%Y-%m-%dT%H:%M'
       ],
-      widget=forms.widgets.DateInput(format="%m/%d/%Y %H:%M")
+      widget= DateTimeInput_forhtml5(format="%Y-%m-%dT%H:%M") ###forms.widgets.DateTimeInput(format="%m/%d/%Y %I:%M %p")
    )
 
    
