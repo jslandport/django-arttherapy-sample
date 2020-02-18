1cbfef7574738f8158e819f5cca3bfc6b0e756a5
KC <54818691+jslandport@users.noreply.github.com>
2020-02-18 11:46:10 -0500

    Update navhelper.py

--- arttherapy/trackart/navhelper.py
+++ arttherapy/trackart/navhelper.py
@@ -1,26 +1,32 @@
 def getnavdictfromparamsdict(dparams, cururl):
    dnav = { }
+   dcrumb = {}
+   dback = {}
    for thiskey in dparams:
       if thiskey == 'art_clientid':
          ## should we link to this client?
          if dparams[thiskey] > 0:
             thislink = '/client/' + str(dparams[thiskey])
             if cururl != thislink:
-               dnav[thislink] = 'This Client'
+               dcrumb[thislink] = 'This Client'
          ## link to the List-of-clients
-         dnav['/viewclients'] = 'All Clients'
+         dback['/viewclients'] = 'All Clients'
       elif thiskey == 'art_appointmentid':
          if dparams[thiskey] > 0:
             thislink = '/appointment/' + str(dparams[thiskey])
             if cururl != thislink:
-               dnav[thislink] = 'This Appointment'
-         dnav['/viewappointments'] = 'All Appointments'
+               dcrumb[thislink] = 'This Appointment'
+         dback['/viewappointments'] = 'All Appointments'
       elif thiskey == 'art_paintingid':
          ## should we link to this painting?
          if dparams[thiskey] > 0 and dparams.get('art_appointmentid') > 0:
             thislink = '/appointment/' + str(dparams['art_appointmentid']) + '/painting/' + str(dparams[thiskey])
             if cururl != thislink:
-               dnav[thislink] = 'This Painting'
+               dcrumb[thislink] = 'This Painting'
          ## link to list-of-paintings
-         dnav['/viewpaintings'] = 'All Paintings'
+         dback['/viewpaintings'] = 'All Paintings'
+   if dcrumb:
+      dnav['dcrumb'] = dcrumb
+   if dback:
+      dnav['dback'] = dback
    return dnav
